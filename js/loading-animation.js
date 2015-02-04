f04f42297eaaefd7cf145c874ac5c6943cf9f07b
Konstantin Mochalov <mochalov_ki@soshace.com>
2015-02-04 16:17:02 +0300

    Working loading animation

--- js/loading-animation.js
+++ js/loading-animation.js
@@ -1,5 +1,30 @@
 $(function() {
-  console.log("yes");
-  console.log($('#loading-bar'));
-  $('#loading-bar').prepend('<img src="img/loading-bar-animation.gif" width="508" height="31" />');
+  var set_active_frame = function(frame) {
+    var y_offset = frame * -31;
+    $("#loading-bar-line").css("background-position", "0 " + y_offset + "px");
+  }
+
+  var run_animation = function(end_callback) {
+    var frame = 0;
+    var last_frame = 20;
+    var text_activation_frames = {13: 1, 20: 2};
+    var next = function() {
+      set_active_frame(frame);
+
+      if (text_activation_frames[frame]) {
+        var index = text_activation_frames[frame];
+        $('#loading-bar > ul > li').eq(index).addClass('enabled');
+      }
+
+      frame++;
+      if (frame > last_frame) {
+        clearInterval(interval);
+        end_callback();
+      }
+    }
+
+    var interval = setInterval(next, 100);
+  }
+
+  run_animation(function(){console.log("End")});
 });
