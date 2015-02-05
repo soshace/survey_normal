c8169c5da11a347bd927f3eea850a62ffe8d2979
Konstantin Mochalov <mochalov_ki@soshace.com>
2015-02-05 11:45:35 +0300

    Added audio player

--- js/loading-animation.js
+++ js/loading-animation.js
@@ -34,4 +34,21 @@ $(function() {
       location.href = "presents_page.html";
     });
   }
+
+  $('.js-player').jPlayer({
+    ready: function () {
+        $(this).jPlayer('setMedia', {
+            mp3: 'audio/uk.mp3'
+        }).jPlayer('play');
+    },
+    swfPath: 'js/vendors/jplayer',
+    supplied: 'mp3',
+    cssSelectorAncestor: '',
+    cssSelector: {
+        play: '.js-play',
+        pause: '.js-pause',
+        playBar: '.js-play-bar',
+        noSolution: '.js-no-solution'
+    }
+  });
 });
