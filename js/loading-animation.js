dadcaaf2fcaf453fd752dbd086cc19353c5dac59
Nikita <nikita-bragin@mail.ru>
2015-02-06 10:36:54 +0400

    style fixes

--- js/loading-animation.js
+++ js/loading-animation.js
@@ -27,28 +27,28 @@ $(function() {
     }
 
   // Set active frame of loading bar
-  var set_active_frame = function(frame) {
-    var y_offset = frame * -31;
-    $("#loading-bar-line").css("background-position", "0 " + y_offset + "px");
+  var setActiveFrame = function(frame) {
+    var yOffset = frame * -31;
+    $("#loading-bar-line").css("background-position", "0 " + yOffset + "px");
   };
 
   // Run loading bar animation, firing callback when finished
-  var run_animation = function(end_callback) {
+  var runAnimation = function(endCallback) {
     var frame = 0;
     var last_frame = 20;
-    var text_activation_frames = {13: 1, 20: 2};
+    var textActivationFrames = {13: 1, 20: 2};
     var next = function() {
-      set_active_frame(frame);
+      setActiveFrame(frame);
 
-      if (text_activation_frames[frame]) {
-        var index = text_activation_frames[frame];
+      if (textActivationFrames[frame]) {
+        var index = textActivationFrames[frame];
         $('#loading-bar > ul > li').eq(index).addClass('enabled');
       }
 
       frame++;
       if (frame > last_frame) {
         clearInterval(interval);
-        end_callback();
+        endCallback();
       }
     };
 
@@ -57,7 +57,7 @@ $(function() {
 
   // Start animation sequence if 'loading-bar' exists on page
   if ($('#loading-bar').length) {
-    run_animation(function(){
+    runAnimation(function(){
       location.href = "presents_page.html";
     });
   }
